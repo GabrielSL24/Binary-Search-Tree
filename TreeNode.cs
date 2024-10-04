448c713a8dc4de69162b0458045b59fa768cc20d
GabrielSL24 <g.soto.1@estudiantec.cr>
2024-10-04 10:15:57 -0600

    first commit, add class BST

---
+++ TreeNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST_en_Disco
+{
+    internal struct TreeNode
+    {
+        int key;
+        public long diskleft;
+        public long diskright; 
+    }
+}
